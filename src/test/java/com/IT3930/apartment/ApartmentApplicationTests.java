0cf4a76123bc4234000d64eb1386872940895d21
LuongDManh <manh.ld180805@gmail.com>
2026-08-30 11:39:14 +0700

    Fix CI

--- src/test/java/com/IT3930/apartment/ApartmentApplicationTests.java
+++ src/test/java/com/IT3930/apartment/ApartmentApplicationTests.java
@@ -2,8 +2,10 @@ package com.IT3930.apartment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApartmentApplicationTests {
 
 	@Test
